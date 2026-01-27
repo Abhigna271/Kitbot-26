6ab5407a4a3ed9148567c7c1aadcd6789ad8f8a9
Abhigna271 <abhigna.bharadwajka@gmail.com>
2026-01-26 23:14:42 -0500

    Fix

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,52 +2,68 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
+import frc.robot.util.SubsystemProfiles;
+
 import java.util.HashMap;
 import java.util.Map;
 import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
-  private IntakeIO m_Io;
+  private IntakeIO m_io;
+  //new IntakeIO called m_io
   public final IntakeInputsAutoLogged m_inputs = new IntakeInputsAutoLogged();
+  //m_inputs is a new logged category
 
-    private frc.robot.util.SubsystemProfiles<IntakeState> m_Profiles;
+  private SubsystemProfiles<IntakeState> m_profiles;
+  //creates the states in intake
 
   public static enum IntakeState {
     kIdle,
     kSpinning,
+    //list of states there
   }
 
   public Intake(IntakeIO intakeIO) {
-    m_Io = intakeIO;
+    //creating an intake that takes in one of the IO files
+    m_io = intakeIO;
     Map<IntakeState, Runnable> periodicHash = new HashMap<>();
     periodicHash.put(IntakeState.kIdle, this::idlePeriodic);
     periodicHash.put(IntakeState.kSpinning, this::spinningPeriodic);
+    //creating map/connecting the variables called periodic hash
+    //Runnable creates "threading"/runs it at the same time
 
-        m_Profiles = new frc.util.SubsystemProfiles<Intake.IntakeState>(periodicHash, IntakeState.kIdle);
-    }
+    m_profiles = new SubsystemProfiles<>(periodicHash, IntakeState.kIdle);
+    //making profiles the hash map, then default state
+  }
 
   @Override
   public void periodic() {
-    m_Io.updateInputs(m_inputs);
-    m_Profiles.getPeriodicFunctionTimed().run();
+    m_io.updateInputs(m_inputs);
+    //update inputs in the 3 classes
+    m_profiles.getPeriodicFunctionTimed().run();
+    //get whatever mapped into current state, runs it
     Logger.processInputs("Intake", m_inputs);
-    Logger.recordOutput("Intake/state", m_Profiles.getCurrentProfile());
+    Logger.recordOutput("Intake/state", m_profiles.getCurrentProfile());
+    //updates and shows
   }
 
   public void idlePeriodic() {
-    m_Io.setVoltage(IntakeConstants.kIdleVoltage.get());
+    m_io.setVoltage(IntakeConstants.kIdleVoltage.get());
+    //What to do during periodic
   }
 
   public void spinningPeriodic() {
-    m_Io.setVoltage(IntakeConstants.kSpinningVoltage.get());
+    m_io.setVoltage(IntakeConstants.kSpinningVoltage.get());
   }
 
   public void updateState(IntakeState state) {
-    m_Profiles.setCurrentProfile(state);
+    m_profiles.setCurrentProfile(state);
+    //allows to switch between states
   }
 
-  public IntakeState getCurrenState() {
-    return m_Profiles.getCurrentProfile();
+  public IntakeState getCurrentState() {
+    return m_profiles.getCurrentProfile();
+    //tells what its in
   }
 
   public double getCurrentVelocity() {
