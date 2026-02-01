a514f15f2f05ac448472d575731b2103ecb9b5b5
DynamicWhiteHat <aahil.syed.usa@gmail.com>
2026-02-01 00:25:52 -0500

    Fixed shooter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Kitbot-26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "6b6753a758cfc8d466fd7d6d8e4ab7f272059692";
-  public static final String GIT_DATE = "2026-01-31 23:55:30 EST";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "e150c3591333e0a130f758a813782222f142b7ad";
+  public static final String GIT_DATE = "2026-02-01 00:07:38 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-01 00:06:27 EST";
-  public static final long BUILD_UNIX_TIME = 1769922387086L;
+  public static final String BUILD_DATE = "2026-02-01 00:22:18 EST";
+  public static final long BUILD_UNIX_TIME = 1769923338973L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
